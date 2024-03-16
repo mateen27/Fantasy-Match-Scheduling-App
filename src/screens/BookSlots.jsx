1f383d51d9f439dd24295ceeec1ca52f0b095944
mateen27 <khanmateen2713@gmail.com>
2024-03-17 01:05:02 +0530

    feat: Slot Booking Screen Header

--- src/screens/BookSlots.jsx
+++ src/screens/BookSlots.jsx
@@ -1,16 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React from 'react';
+import { ScrollView } from 'react-native';
+import { NativeBaseProvider, Box, Heading, Text, Center, HStack, Spacer } from 'native-base';
+import { LinearGradient } from 'expo-linear-gradient';
+import { useNavigation } from '@react-navigation/native';
 
 const BookSlots = (matchInfo) => {
-    console.log(matchInfo)
-  return (
-    
-    <View>
-      <Text>BookSlots</Text>
-    </View>
-  )
-}
+    // console.log(matchInfo.route.params.team1);
+    const navigation = useNavigation()
+    return (
+        <NativeBaseProvider>
+            <ScrollView style={{ flex: 1 }}>
+                <Box flex={1}>
+                    <LinearGradient
+                        colors={['#4e54c8', '#8f94fb']}
+                        start={[0, 0]}
+                        end={[1, 1]}
+                        style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}
+                    >
+                        <Heading my={'10%'} color="white" mb={4}>
+                            Match Information
+                        </Heading>
+                        <HStack space={4}>
+                            <Text color="white" fontSize="xl" fontWeight={'semibold'}>
+                                {matchInfo.route.params.team1}
+                            </Text>
+                            <Spacer />
+                            <Text color="white" fontSize="xl" fontWeight={'semibold'}>
+                            {matchInfo.route.params.team2}
+                            </Text>
+                        </HStack>
+                    </LinearGradient>
+                </Box>
+            </ScrollView>
+        </NativeBaseProvider>
+    );
+};
 
-export default BookSlots
-
-const styles = StyleSheet.create({})
+export default BookSlots;
