ea535c328195f5a64f98701cf18df0e7f84b7dcf
mateen27 <khanmateen2713@gmail.com>
2024-03-17 16:23:51 +0530

    feat: user profile implemented

--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,14 +1,70 @@
-import { StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import { Alert } from 'react-native';
+import { NativeBaseProvider, Avatar, Box, Button, Center, HStack, Heading, Text, VStack, Card, Pressable } from 'native-base';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useNavigation } from '@react-navigation/native';
 
 const Profile = () => {
+  const [userData, setUserData] = useState('');
+
+  const navigation = useNavigation();
+
+  useEffect(() => {
+    fetchUserData();
+  }, []);
+
+  const fetchUserData = async () => {
+    try {
+      const encryptedUserData = await AsyncStorage.getItem('user');
+      if (encryptedUserData) {
+        const userData = JSON.parse(encryptedUserData);
+        setUserData(userData);
+      } else {
+        console.log('User data not found');
+      }
+    } catch (error) {
+      console.error('Error fetching user data:', error);
+      Alert.alert('Error fetching user data:', error);
+    }
+  };
+
   return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+    <NativeBaseProvider>
+      <Center>
+        <Avatar
+          my={4}
+          bg="purple.600"
+          size="2xl"
+          source={{
+            uri: "https://images.unsplash.com/photo-1510771463146-e89e6e86560e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80"
+          }}
+        >
+          RB
+        </Avatar>
+        <Heading size={'lg'}>{userData.name}</Heading>
+        <Heading size={'sm'} color={'gray.500'} my={1}>{userData.email}</Heading>
+      </Center>
+      <VStack bg={'#f8f8ff'} padding={5} my={2} justifyContent="space-between">
+        <Box>
+          <Text fontWeight={'bold'}>Account Settings</Text>
+        </Box>
 
-export default Profile
+      </VStack>
+      <VStack bg={'#f8f8ff'} padding={5} my={2} justifyContent="space-between">
+        <Pressable onPress={() => console.log('hi')}>
+          <Box>
+          <Text fontWeight={'bold'} onPress={() => navigation.navigate('FixedMatches')}>Fixed Matches</Text>
+        </Box>
+        </Pressable>
 
-const styles = StyleSheet.create({})
+      </VStack>
+      <VStack my={10}>
+        <Center>
+          <Button bgColor= { '#6167ce'} size={'lg'}>Sign out</Button>
+        </Center>
+      </VStack>
+    </NativeBaseProvider>
+  );
+};
+
+export default Profile;
